cd7d8044af7d0270081ff14a83c4b0ed768200b8
Ross McIlwaine <rdm0423@gmail.com>
2015-03-06 16:59:32 -0700

    Creates CardViewController

---
+++ cardalot/CardViewController.m
@@ -0,0 +1,37 @@
+//
+//  CardViewController.m
+//  cardalot
+//
+//  Created by Ross McIlwaine on 3/6/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "CardViewController.h"
+
+@interface CardViewController ()
+
+@end
+
+@implementation CardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
