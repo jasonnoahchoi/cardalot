2005cd6eff40fe869fa91253df063dbbe0b9ae0e
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-06 15:19:06 -0700

    Initial Commit

---
+++ cardalot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cardalot
+//
+//  Created by Jason Noah Choi on 3/6/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cardalot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cardalot
+//
+//  Created by Jason Noah Choi on 3/6/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
