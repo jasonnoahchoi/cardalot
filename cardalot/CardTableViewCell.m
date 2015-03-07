4229d4f2c5f4295228619ecf7e4589f945f693c3
Ross McIlwaine <rdm0423@gmail.com>
2015-03-06 18:24:52 -0700

    Adds CardTableViewCell

---
+++ cardalot/CardTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CardTableViewCell.m
+//  cardalot
+//
+//  Created by Ross McIlwaine on 3/6/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "CardTableViewCell.h"
+
+@implementation CardTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
