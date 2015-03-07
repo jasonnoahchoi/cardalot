967e451ed3e0b80eb6a46ecacb114db7dc9af62c
arbmos <oscarecho@fastmail.fm>
2015-03-06 21:22:50 -0700

    Update data source methods to use data from DeckController

--- cardalot/DeckCollectionViewDataSource.m
+++ cardalot/DeckCollectionViewDataSource.m
@@ -7,7 +7,7 @@
 //
 
 #import "DeckCollectionViewDataSource.h"
-//#import "DeckController.h"
+#import "DeckController.h"
 
 static NSString * const cellIdentifier = @"cell";
 
@@ -21,32 +21,26 @@ static NSString * const cellIdentifier = @"cell";
     UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
     
     cell.contentView.backgroundColor = [UIColor cyanColor];
+    NSInteger index = [DeckController sharedInstance].decks.count;
     
-    switch (indexPath.item) {
-        case 50: {
-            UILabel *lastCellLabel = [UILabel new];
-            lastCellLabel.text = @"#";
-            lastCellLabel.textAlignment = NSTextAlignmentCenter;
-            [cell.contentView addSubview:lastCellLabel];
-            [lastCellLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-            NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(lastCellLabel);
-            [cell.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[lastCellLabel]|" options:0 metrics:0 views:viewDictionary]];
-            [cell.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[lastCellLabel]|" options:0 metrics:0 views:viewDictionary]];
-            
-            return cell;
-            break;
-        }
-        default: {
-            return cell;
-        }
+    if (indexPath.item == index) {
+        UILabel *lastCellLabel = [UILabel new];
+        lastCellLabel.text = @"#";
+        lastCellLabel.textAlignment = NSTextAlignmentCenter;
+        [cell.contentView addSubview:lastCellLabel];
+        [lastCellLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+        NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(lastCellLabel);
+        [cell.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[lastCellLabel]|" options:0 metrics:0 views:viewDictionary]];
+        [cell.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[lastCellLabel]|" options:0 metrics:0 views:viewDictionary]];
+    
+        return cell;
+    } else {
+        return cell;
     }
-//    return cell;
-    
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-//    return [DeckController sharedInstance].decks.count;
-    return 51;
+    return [DeckController sharedInstance].decks.count + 1;
 }
 
 @end
