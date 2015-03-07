ac2e5d0d71332c3cf7930646b05c4e845558c24a
arbmos <oscarecho@fastmail.fm>
2015-03-06 17:16:44 -0700

    Add model objects

---
+++ cardalot/Deck.m
@@ -0,0 +1,17 @@
+//
+//  Deck.m
+//  cardalot
+//
+//  Created by sombra on 2015-03-06.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "Deck.h"
+
+
+@implementation Deck
+
+@dynamic nameTag;
+@dynamic cards;
+
+@end
