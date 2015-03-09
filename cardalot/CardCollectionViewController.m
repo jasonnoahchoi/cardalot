9a11c722bbd3d5265dbba358d44ba0159bb05d70
arbmos <oscarecho@fastmail.fm>
2015-03-08 18:44:30 -0600

    Add Edit UIBarButtonItem to the navigation bar of CardCollectionViewController to toggle editing mode

--- cardalot/CardCollectionViewController.m
+++ cardalot/CardCollectionViewController.m
@@ -8,6 +8,7 @@
 
 #import "CardCollectionViewController.h"
 #import "CardCollectionViewDataSource.h"
+#import "CardCollectionViewCell.h"
 #import <MTCardLayout/MTCardLayout.h>
 #import <MTCardLayout/UICollectionView+CardLayout.h>
 
@@ -23,6 +24,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Edit" style:UIBarButtonItemStylePlain target:self action:@selector(toggleEditingMode:)];
+    
     MTCardLayout *cardLayout = [[MTCardLayout alloc] init];
     
     self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:cardLayout];
@@ -34,6 +37,27 @@
     [self.dataSource registerCollectionView:self.collectionView];
     
     self.collectionView.delegate = self;
+}
+
+#pragma mark IBAction
+- (IBAction)toggleEditingMode:(id)sender {
+    UIBarButtonItem *barButtonItem = sender;
+    
+    CardCollectionViewCell *cell = (CardCollectionViewCell *)[self.collectionView cellForItemAtIndexPath:[[self.collectionView indexPathsForSelectedItems] firstObject]];
+    
+    if (self.isEditing) {
+        [cell.textField setEnabled:NO];
+        cell.textView.editable = NO;
+        
+        [barButtonItem setTitle:@"Edit"];
+        [self setEditing:NO animated:YES];
+    } else {
+        [cell.textField setEnabled:YES];
+        cell.textView.editable = YES;
+        
+        [barButtonItem setTitle:@"Done"];
+        [self setEditing:YES animated:YES];
+    }
 }
 
 #pragma mark UICollectionViewDelegate
