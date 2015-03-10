bab12f0dd02d319bba0b5dc4726c3e60ae75529a
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-10 16:43:34 -0600

    Add a view controller that controls the view

---
+++ cardalot/StudyViewController.h
@@ -0,0 +1,13 @@
+//
+//  StudyViewController.h
+//  cardalot
+//
+//  Created by Jason Noah Choi on 3/10/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StudyViewController : UIViewController
+
+@end
