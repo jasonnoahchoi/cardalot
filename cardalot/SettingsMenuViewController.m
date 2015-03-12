0e26b108e7f76269614ccfdba74080d69c738833
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-03-12 17:38:55 -0600

    Creates Settings Menu ViewController

---
+++ cardalot/SettingsMenuViewController.m
@@ -0,0 +1,45 @@
+//
+//  SettingsMenuViewController.m
+//  cardalot
+//
+//  Created by Ross McIlwaine on 3/12/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "SettingsMenuViewController.h"
+#import "DeckCollectionViewController.h"
+
+@interface SettingsMenuViewController ()
+
+@end
+
+@implementation SettingsMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu_icon"] style:UIBarButtonItemStylePlain target:self action:@selector(open)];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
