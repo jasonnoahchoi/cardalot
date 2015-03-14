97578ded7926472ed5a8df625096d4539b9d62e3
Ross McIlwaine <RossMcIlwaine@Rosss-MacBook-Air.local>
2015-03-14 00:25:40 -0600

    Sets tableview framework for Settings

--- cardalot/SettingsMenuViewController.m
+++ cardalot/SettingsMenuViewController.m
@@ -10,7 +10,9 @@
 #import "DeckCollectionViewController.h"
 #import <MMDrawerController.h>
 
-@interface SettingsMenuViewController ()
+@interface SettingsMenuViewController () <UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -27,6 +29,45 @@
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menu_icon"] style:UIBarButtonItemStylePlain target:self action:@selector(open)];
     
     
+    // Create tableView
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    
+    // Datasource
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+    // Background Color of TableView
+//    self.tableView.backgroundColor = [UIColor colorWithRed:0.25 green:0.25 blue:0.25 alpha:1];
+    
+    
+    // Add to view
+    [self.view addSubview:self.tableView];
+    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSArray *menuListArray = @[@"Advanced Settings", @"Support", @"Legal"];
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    cell.backgroundColor = [UIColor colorWithRed:0.25 green:0.25 blue:0.25 alpha:1];
+    NSAttributedString *attText = [[NSAttributedString alloc]initWithString:menuListArray[indexPath.row - 2] attributes:@{NSForegroundColorAttributeName:[UIColor whiteColor], NSFontAttributeName:[UIFont fontWithName:@"Marion-Bold" size:16]}];
+    cell.textLabel.attributedText = attText;
+    // removes highlighting of cells when selecting
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+    
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
 }
 
