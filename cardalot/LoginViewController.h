22f6c2682492c0f6f8de0d7c876d0c8e0c28b393
Jason Choi <jasonnoahchoi@gmail.com>
2015-03-16 01:59:21 -0600

    Add login view controller, use category for color on appearancecontroller, add errorhandler, settings controller, and logo

---
+++ cardalot/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  cardalot
+//
+//  Created by Jason Noah Choi on 3/15/15.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
