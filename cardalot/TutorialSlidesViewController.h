43f097c4aca6243496922a9867f480a9381a8087
arbmos <oscarecho@fastmail.fm>
2015-03-19 20:25:30 -0600

    Add view controller for presenting tutorial slides

---
+++ cardalot/TutorialSlidesViewController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialSlidesViewController.h
+//  cardalot
+//
+//  Created by sombra on 2015-03-19.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TutorialSlidesViewController : UIViewController
+
+@end
---
+++ cardalot/TutorialSlidesViewController.m
@@ -0,0 +1,22 @@
+//
+//  TutorialSlidesViewController.m
+//  cardalot
+//
+//  Created by sombra on 2015-03-19.
+//  Copyright (c) 2015 Jason Choi. All rights reserved.
+//
+
+#import "TutorialSlidesViewController.h"
+
+@interface TutorialSlidesViewController ()
+
+@end
+
+@implementation TutorialSlidesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
